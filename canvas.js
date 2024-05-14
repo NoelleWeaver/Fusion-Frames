4aad1601f64adf4848ff7041feee9b398ba69161
Daniel <141285622+DanielFreeman068@users.noreply.github.com>
2024-05-13 23:39:27 -0700

    sabrina autism

--- canvas.js
+++ canvas.js
@@ -1,3 +1,14 @@
+clickCounter();
+
+function clickCounter() {
+    if (localStorage.clickcount) {
+        localStorage.clickcount = Number(localStorage.clickcount)+1;
+    } else {
+        localStorage.clickcount = 1;
+    }
+    document.getElementById("demo").innerHTML = localStorage.clickcount;
+}
+
 var canvas = document.querySelector('canvas');
 
 canvas.width = window.innerWidth;
@@ -5,45 +16,45 @@ canvas.height = window.innerHeight;
 
 var c = canvas.getContext('2d');
 
-// c.fillStyle = "red";
-// c.fillRect(100,100,100,100);
-// c.fillStyle = "blue"
-// c.fillRect(400,100,100,100);
-// c.fillStyle = "green";
-// c.fillRect(300,300,100,100);
-// console.log(canvas);
+// // c.fillStyle = "red";
+// // c.fillRect(100,100,100,100);
+// // c.fillStyle = "blue"
+// // c.fillRect(400,100,100,100);
+// // c.fillStyle = "green";
+// // c.fillRect(300,300,100,100);
+// // console.log(canvas);
 
-//Line
-// c.beginPath();
-// c.moveTo(50,300);
-// c.lineTo(300, 100);
-// c.lineTo(400, 300);
-// c.strokeStyle = "hotpink";
-// c.stroke();
+// //Line
+// // c.beginPath();
+// // c.moveTo(50,300);
+// // c.lineTo(300, 100);
+// // c.lineTo(400, 300);
+// // c.strokeStyle = "hotpink";
+// // c.stroke();
 
-//Arc/Circle
-// c.beginPath();
-// c.arc(300,300,30,0, Math.PI * 2, false);
-// c.strokeStyle = "blue";
-// c.stroke();
+// //Arc/Circle
+// // c.beginPath();
+// // c.arc(300,300,30,0, Math.PI * 2, false);
+// // c.strokeStyle = "blue";
+// // c.stroke();
 
 // for (var i = 0; i < 10; i++){
-//     var x = Math.random() * window.innerWidth;
-//     var y = Math.random() * window.innerHeight;
+//     var a = Math.random() * window.innerWidth;
+//     var b = Math.random() * window.innerHeight;
 //     c.beginPath();
-//     c.arc(x,y,30,0, Math.PI * 2, false);
+//     c.arc(a,b,30,0, Math.PI * 2, false);
 //     c.strokeStyle = "blue";
 //     c.stroke();
 // }
 
 var x = Math.random() * innerWidth;
-var dx = (Math.random() - 0.5) * 40;
+var dx = (Math.random() - 0.5) * 100;
 var y = Math.random() * innerHeight;
-var dy = (Math.random() - 0.5) * 40;
+var dy = (Math.random() - 0.5) * 100;
 var radius = 30;
 function animate() {
     requestAnimationFrame(animate);
-    c.clearRect(0,0,innerWidth,innerHeight);
+    // c.clearRect(0,0,innerWidth,innerHeight);
     c.beginPath();
     c.arc(x,y,50,0, Math.PI * 2, false);
     c.strokeStyle = "blue";
@@ -56,6 +67,14 @@ function animate() {
     }
     x+=dx;
     y+=dy;
+    // for(var i = 0; i < 10; i++){
+    //     var a = Math.random() * window.innerWidth;
+    //     var b = Math.random() * window.innerHeight;
+    //     c.beginPath();
+    //     c.arc(a,b,30,0, Math.PI * 2, false);
+    //     c.strokeStyle = "blue";
+    //     c.stroke();
+    // }
 }
 
 animate();
