b52a208bc7f46ac18d03338bcee027009e89f794
Alan Sanchez <asanch317@west-mec.org>
2024-05-13 17:01:18 -0700

    Pushing harder

--- about.js
+++ about.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', function () {
     const sidebar = document.querySelector('.sidebar');
     const toggleButton = document.querySelector('.sidebar-toggle');
-    const sliderContainer = document.querySelector('.slider-container');
+    const sliderContainer = document.querySelector('.content');
     const logoPart1 = document.querySelector('.logo-part1');
     const originalBackground = 'url(https://coolbackgrounds.io/images/backgrounds/white/white-canyon-6c5d2a4c.jpg)';
     const alternateBackground = 'url(https://t3.ftcdn.net/jpg/01/36/78/52/360_F_136785269_05hcRhonbg4cyLZFApZWemV7oypynXqd.jpg)';
@@ -11,7 +11,8 @@ document.addEventListener('DOMContentLoaded', function () {
         toggleButton.innerHTML = sidebar.classList.contains('closed') ? '>' : '<';
         
         toggleButton.style.left = sidebar.classList.contains('closed') ? '0px' : originalSidebarWidth;
-        
+
+        sliderContainer.style.paddingLeft = sidebar.classList.contains('closed') ? '0px' : originalSidebarWidth;
         
         logoPart1.style.backgroundImage = sidebar.classList.contains('closed') ? alternateBackground : originalBackground;
     }
